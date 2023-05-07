8c66b6529c0895ba24e9aa850317f7fb421afdac
AleBradC <alexandrabradclaudia@gmail.com>
2023-05-07 13:01:39 +0300

    added tests

--- server/src/tests/unit/routes/register.test.ts
+++ server/src/tests/unit/routes/register.test.ts
@@ -1,16 +1,7 @@
 import request from "supertest";
 import app from "../../../app";
-import connectDB from "../../../dataBase/connectionDB";
 
 describe("registerRoute", () => {
-  beforeAll(async () => {
-    await connectDB.initialize();
-  });
-
-  afterAll(async () => {
-    await connectDB.destroy();
-  });
-
   it("should return 200 and the response from AuthService.register", async () => {
     const response = await request(app).post("/api/register").send({
       user_name: "test",
